8cd0bbee23ec6af6112bb5222588de740321e784
cmstyles3 <77931245+cmstyles3@users.noreply.github.com>
2025-06-16 20:54:17 -0500

    update

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+class_name PlayerController
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 10.0
+@export var jump_power = 10.0
+
+var jump_multiplier = -10.0
+var speed_multiplier = 10.0
+var direction = 0
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,18 +19,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
 		
 	if Input.is_key_pressed(KEY_0):
 		position.y = -500
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speed_multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
