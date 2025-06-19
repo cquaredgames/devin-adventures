237498fedf28f28a63c02aa55dde0752daabd05a
cmstyles3 <77931245+cmstyles3@users.noreply.github.com>
2025-06-19 12:04:23 -0500

    update

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -9,18 +9,21 @@ var jump_multiplier = -10.0
 var speed_multiplier = 10.0
 var direction = 0
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-
-
+func _input(event: InputEvent) -> void:
+	# Handle jump.
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+		
+	# Handle jump down
+	if event.is_action_pressed("jump_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
+		velocity += get_gravity() * delta	
 		
 	if Input.is_key_pressed(KEY_0):
 		position.y = -500
